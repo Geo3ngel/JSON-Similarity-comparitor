27e469744dfe19a0ec314e42f30dae9095aac270
GeoEngel <GeoEngel.z@gmail.com>
2019-10-21 00:14:33 -0600

    Added Comparison class for holding json file info

---
+++ src/Comparison.py
@@ -0,0 +1,36 @@
+# The purpose of this Class is to hold relevant JSON objects for purpose of comparison with reference
+# To their respective file names.
+
+# Imports
+import Equivalence_Processor as eq
+import json
+
+class Comparison():
+    
+    # Initialize a comparison with the json file name.
+    def __init__(self, json_filename_a, json_filename_b):
+        print("Initialized new comparison")
+        json_object_a, json_object_b = self.open_files_as_objects(json_filename_a, json_filename_b)
+        eq.compare_json_objs(json_object_a, json_object_b)
+    
+    # Converts files to objects
+    def open_files_as_objects(self, filename_A, filename_B):
+        json_object_a = None
+        json_object_b = None
+        
+        # Opens both JSON files and stores them as objects
+        with open(filename_A) as json_file:
+            json_object_a = json.load(json_file)
+            
+        with open(filename_B) as json_file:
+            json_object_b = json.load(json_file)
+            
+        # Temporary print outs for the objects. Include in Verbos call?
+        for p in json_object_a:
+            print(p)
+        print()
+        for p in json_object_b:
+            print(p)
+        
+        # Return both json objects
+        return json_object_a, json_object_b
--- src/JSON_Compare.py
+++ src/JSON_Compare.py
@@ -1,9 +1,7 @@
+# Imports
 import sys
-import json
 import os.path
-import Equivalence_Processor as eq
-
-# TODO: Command line arguements for files to compare.
+from Comparison import Comparison
 
 # effectively the program's main function.
 def run():
@@ -16,6 +14,7 @@ def run():
         # Validate that both the json files entered as parameters exist.
         if validate_file(json_file_A) and validate_file(json_file_B):
             print("Running", sys.argv[0], " on files:", json_file_A, "and", json_file_B)
+            comp = Comparison(json_file_A, json_file_B)
         else:
             print("Please enter valid file paths. Program Exiting...")
     else:
@@ -33,25 +32,5 @@ def validate_file(file_path):
         print("File name/path is Invalid:", file_path)
         return False
         
-# Converts files to objects
-def open_files_as_objects(filename_A, filename_B):
-    json_object_a = None
-    json_object_b = None
-    # TODO: Open both JSON files and store as objects
-    with open(filename_A) as json_file:
-        json_object_a = json.load(json_file)
-        
-    with open(filename_B) as json_file:
-        json_object_b = json.load(json_file)
-        
-    # Temporary print outs for the objects. Include in Verbos call?
-    for p in json_object_a:
-        print(p)
-    print()
-    for p in json_object_b:
-        print(p)
-        
-    eq.compare_json_objs(json_object_a, json_object_b)
-
 run()
 # Current testing command: python JSON_Compare.py ../Examples/BreweriesMaster.json ../Examples/BreweriesSample1.json
