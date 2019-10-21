158b7df82eeaada7c31bb5a676aeabc7d83f079c
GeoEngel <GeoEngel.z@gmail.com>
2019-10-21 00:19:51 -0600

    Added localized variable storage in Comparison Object

--- src/Comparison.py
+++ src/Comparison.py
@@ -7,11 +7,21 @@ import json
 
 class Comparison():
     
-    # Initialize a comparison with the json file name.
+    # Initialize a comparison with the json file name
     def __init__(self, json_filename_a, json_filename_b):
         print("Initialized new comparison")
-        json_object_a, json_object_b = self.open_files_as_objects(json_filename_a, json_filename_b)
-        eq.compare_json_objs(json_object_a, json_object_b)
+        # Store the file names
+        self.filename_a = json_filename_a
+        self.filename_b = json_filename_b
+        
+        # Store the json objects for easier referencing
+        self.json_object_a, self.json_object_b = self.open_files_as_objects(json_filename_a, json_filename_b)
+        
+        # TODO: Initialize similarity metrics
+        # TODO: Consider mapping of filename/objects and related information?
+        
+        # Run comparison on the json objects
+        eq.compare_json_objs(self.json_object_a, self.json_object_b)
     
     # Converts files to objects
     def open_files_as_objects(self, filename_A, filename_B):
